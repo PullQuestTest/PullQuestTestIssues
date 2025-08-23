2b9172181f57b3515cf8aa37a5d9a14b84a7603a
NishantSinghhhhh <nishantsingh_230137@aitpune.edu.in>
2025-08-24 03:03:21 +0530

    Added code
    Signed-off-by: NishantSinghhhhh <nishantsingh_230137@aitpune.edu.in>

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
     <div className="flex flex-col gap-4 items-center justify-center h-screen">
       <h1 className="text-xl font-medium">Welcome to GoPAY!!</h1>
       <div className="flex gap-6 w-sm items-center justify-center">
-        <Button to="/signin" btnText="Sign In" />
+        {/* <Button to="/signin" btnText="Sign In" /> */}
         <Button to="/signup" btnText="Sign Up" />
       </div>
     </div>
