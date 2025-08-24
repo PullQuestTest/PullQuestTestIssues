76f8e28a65f7671c8c74acfba90d456a0890a6a9
Arun Kumar Kushwaha <arunkushwaha_230604@aitpune.edu.in>
2025-08-24 14:21:43 +0530

    Update Balance.jsx

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Balance = ({ balance }) => {
-  return <div className="text-xl font-semibold">Your Balance: Rs {balance}</div>;
+  return <div className="text-xl font-medium">Your Balance: Rs {balance}</div>;
 };
 
 export default Balance;
