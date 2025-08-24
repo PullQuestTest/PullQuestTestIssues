52d411030499962e7c2a7e5f564572cfaae487a5
Arun Kumar Kushwaha <arunkushwaha_230604@aitpune.edu.in>
2025-08-24 14:22:21 +0530

    Update SubHeading.jsx

--- src/components/SubHeading.jsx
+++ src/components/SubHeading.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SubHeading = ({ label }) => {
-  return <div className="text-slate-500 font-md">{label}</div>;
+  return <div className="text-slate-500 font-medium">{label}</div>;
 };
 
 export default SubHeading;
